d7db0d2c4dffbe50a7edc78f7f41560507e2e9b5
anproagile <anproagile@gmail.com>
2020-02-20 16:14:39 +0100

    GA: fix typo

--- apps/commercetools/src/AppConfig/FieldSelector/index.tsx
+++ apps/commercetools/src/AppConfig/FieldSelector/index.tsx
@@ -1,22 +1,26 @@
-import * as React from 'react';
-import get from 'lodash/get';
-import set from 'lodash/set';
-import omit from 'lodash/omit';
-import tokens from '@contentful/forma-36-tokens';
-import { css } from 'emotion';
-import { Form, Subheading, CheckboxField } from '@contentful/forma-36-react-components';
+import * as React from "react";
+import get from "lodash/get";
+import set from "lodash/set";
+import omit from "lodash/omit";
+import tokens from "@contentful/forma-36-tokens";
+import { css } from "emotion";
+import {
+  Form,
+  Subheading,
+  CheckboxField
+} from "@contentful/forma-36-react-components";
 
-import { ToggleGroup } from './ToggleGroup';
-import { ContentType, CompatibleFields, FieldsConfig } from '../fields';
-import { PickerMode } from '../../interfaces';
+import { ToggleGroup } from "./ToggleGroup";
+import { ContentType, CompatibleFields, FieldsConfig } from "../fields";
+import { PickerMode } from "../../interfaces";
 
 const styles = {
   fieldGroup: css({
-    display: 'flex',
-    flexDirection: 'column'
+    display: "flex",
+    flexDirection: "column"
   }),
   select: css({
-    marginLeft: '10px'
+    marginLeft: "10px"
   })
 };
 
@@ -38,7 +42,11 @@ export default class FieldSelector extends React.Component<Props> {
 
     this.props.onSelectedFieldsChange(
       isTargetChecked
-        ? set(Object.assign({}, this.props.selectedFields), [ctId, fieldId], type)
+        ? set(
+            Object.assign({}, this.props.selectedFields),
+            [ctId, fieldId],
+            type
+          )
         : omit(this.props.selectedFields, [`${ctId}.${fieldId}`])
     );
   };
@@ -67,14 +75,29 @@ export default class FieldSelector extends React.Component<Props> {
                   <CheckboxField
                     id={`field-box-${ct.sys.id}-${field.id}`}
                     labelText={field.name}
-                    helpText={`Short text · Field ID: ${field.id}`}
+                    helpText={`${
+                      field.type === "Symbol"
+                        ? "Short text"
+                        : "Short text, list"
+                    } · Field ID: ${field.id}`}
                     checked={isChecked}
-                    onChange={this.onSelectedFieldChange.bind(this, ct.sys.id, field.id, 'product')}
+                    onChange={this.onSelectedFieldChange.bind(
+                      this,
+                      ct.sys.id,
+                      field.id,
+                      "product"
+                    )}
                   />
                   {isChecked && (
                     <ToggleGroup
                       activePickerMode={type}
-                      onChange={type => this.onSelectedFieldTypeChange(ct.sys.id, field.id, type)}
+                      onChange={type =>
+                        this.onSelectedFieldTypeChange(
+                          ct.sys.id,
+                          field.id,
+                          type
+                        )
+                      }
                     />
                   )}
                 </div>
