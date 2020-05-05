1a1e5e512032b2ad54d393dde939853ed8a6011e
anproagile <anproagile@gmail.com>
2020-05-05 05:38:17 -0700

    Remove example config screen code

--- apps/commercetools/src/renderDialog.ts
+++ apps/commercetools/src/renderDialog.ts
@@ -4,8 +4,7 @@ import { Hash } from './interfaces';
 import { DialogExtensionSDK } from 'contentful-ui-extensions-sdk';
 
 export async function renderDialog(sdk: DialogExtensionSDK) {
-  const { projectKey, locale, clientId, clientSecret, apiEndpoint, authApiEndpoint } = sdk
-    .parameters.installation as Hash;
+  const { projectKey, locale, clientId, clientSecret } = sdk.parameters.installation as Hash;
 
   const ID = 'dialog-root';
   const container = document.createElement('div');
@@ -19,12 +18,6 @@ export async function renderDialog(sdk: DialogExtensionSDK) {
   const pickerModeIsCategory = get(sdk, ['parameters', 'invocation', 'pickerMode']) === 'category';
 
   const pickerOptions = {
-    platform: {
-      authUri: authApiEndpoint,
-      apiUri: apiEndpoint,
-      graphQLUri: apiEndpoint,
-      mcUri: apiEndpoint.replace('https://api', 'https://mc')
-    },
     project: {
       projectKey,
       credentials: {
